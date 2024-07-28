19112c928e15d0ac0113e5b0a08b2db194868880
Tilavuz <tilav777@gmail.com>
2024-07-29 00:03:10 +0500

    ....

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,12 +1,14 @@
 const { generateToken } = require("../helper/generate-token");
 const Auth = require("../models/auth.model");
+const User = require("../models/user.model");
+const Teacher = require("../models/teacher.model");
 const bcrypt = require("bcrypt");
 
 const login = async (req, res) => {
   try {
     const { phone, password } = req.body;
-    const auth = await Auth.findOne({ phone })
-    
+    const auth = await Auth.findOne({ phone });
+
     if (!auth) throw new Error("Bunday admin mavjut emas!");
 
     const isMatch = await bcrypt.compare(password, auth.password);
@@ -15,7 +17,27 @@ const login = async (req, res) => {
     const token = generateToken({ _id: auth._id });
     res.json({ token, auth });
   } catch (error) {
-    res.json({ message: error.message })
+    res.json({ message: error.message });
+  }
+};
+
+const loginTeacher = async (req, res) => {
+  try {
+    const { phone, password } = req.body;
+    const auth = await Auth.findOne({ phone, role: "teacher" });
+
+    if (!auth) throw new Error("Bunday ustoz mavjut emas!");
+
+    const isMatch = await bcrypt.compare(password, auth.password);
+    if (!isMatch) throw new Error("Parolda xatolik bor!");
+
+    const user = await Teacher.findOne({ auth: auth._id })
+      .populate("auth")
+      .populate("filial");
+    const token = generateToken({ _id: auth._id });
+    res.json({ token, user });
+  } catch (error) {
+    res.json({ message: error.message });
   }
 };
 
@@ -37,11 +59,23 @@ const register = async (req, res) => {
 const getAuth = async (req, res) => {
   try {
     const { _id } = req.user;
-    const auth = await Auth.findById(_id).select('-password')
+    const auth = await Auth.findById(_id).select("-password");
     res.json(auth);
   } catch (error) {
     res.json({ message: error.message });
   }
 };
 
-module.exports = { login, register, getAuth };
+const getUser = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    const user = await Teacher.findOne({ auth: _id })
+      .populate("auth")
+      .populate("filial");
+    res.json(user);
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+};
+
+module.exports = { login, register, getAuth, loginTeacher, getUser };
--- routers/auth.router.js
+++ routers/auth.router.js
@@ -1,10 +1,12 @@
-const { getAuth, login, register } = require('../controllers/auth.controller')
+const { getAuth, login, register, loginTeacher, getUser } = require('../controllers/auth.controller')
 const { isAuth } = require('../middleware/auth.middleware')
 
 const router = require('express').Router()
 
 router.get('/auth', isAuth, getAuth)
+router.get("/auth/user", isAuth, getUser);
 router.post('/auth/login', login)
+router.post("/auth/login/teacher", loginTeacher);
 router.post("/auth/register", register);
 
 
